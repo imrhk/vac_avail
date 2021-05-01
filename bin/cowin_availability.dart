f569838a3c43e24b8873b94b52e947632f29e884
Rahul Kumar <rahul.cs.mnnit@gmail.com>
2021-05-02 01:25:32 +0530

    Initial commit

---
+++ bin/cowin_availability.dart
@@ -0,0 +1,5 @@
+import 'package:cowin_availability/cowin_availability.dart' as cowin_availability;
+
+void main(List<String> arguments) async{
+  await cowin_availability.getAvailability();
+}
---
+++ lib/cowin_availability.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+const district_id = 188;
+Future<Null> getAvailability() async {
+  final now = DateTime.now();
+  final localNow = now.add(Duration(
+      hours: now.timeZoneOffset.inHours + 5,
+      minutes: now.timeZoneOffset.inMinutes + 30));
+
+  await Future.wait(Iterable.generate(4)
+      .map((e) => _getAvailabilityFromDate(localNow.add(Duration(days: 7))))
+      .toList());
+}
+
+Future<Null> _getAvailabilityFromDate(DateTime localTime) async {
+  final localDay =
+      localTime.day > 9 ? '0${localTime.day}' : localTime.day.toString();
+  final localMonth =
+      localTime.month > 9 ? '0${localTime.month}' : localTime.month.toString();
+  final localYear = localTime.year.toString();
+
+  final url = Uri.parse(
+      'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=$district_id&date=$localDay-$localMonth-$localYear');
+
+  final response =
+      await http.get(url.toString(),  headers: {
+    'authority': 'cdn-api.co-vin.in',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'sec-ch-ua':
+        '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+    'accept': 'application/json, text/plain, */*',
+    'sec-ch-ua-mobile': '?0',
+    'user-agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+    'origin': 'https://www.cowin.gov.in',
+    'sec-fetch-site': 'cross-site',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://www.cowin.gov.in/',
+    'accept-language': 'en-US,en;q=0.9,es-ES;q=0.8,es;q=0.7',
+  });
+
+  final output = StringBuffer();
+  final map = jsonDecode(response.body) as Map;
+  final availablePlaces = (map['centers'] as List)
+      .where((e) => (e['sessions'] as List)
+          .where((e) => e['available_capacity'] > 0 && e['min_age_limit'] == 18)
+          .toList()
+          .isNotEmpty)
+      .toList();
+  availablePlaces.forEach((e) {
+    output.write(e['name']);
+    (e['sessions'] as List)
+        .where((e) => e['available_capacity'] > 0 && e['min_age_limit'] == 18)
+        .forEach((e) {
+      output.write('${e['date']} -> ${e['slots']}');
+    });
+    output.write('\n');
+  });
+  final result = output.toString();
+  if(result.isNotEmpty) {
+    print(result);
+  }
+}
